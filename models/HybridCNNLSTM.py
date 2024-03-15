c7af627eb2ed4cdb7e48af40711a1088660d178b
Timothy Do <timothydobsa@gmail.com>
2024-03-15 10:20:27 -0700

    used stack architecture again.

--- models/HybridCNNLSTM.py
+++ models/HybridCNNLSTM.py
@@ -56,9 +56,9 @@ class HybridCNNLSTM(nn.Module):
         # )
 
         self.lstm_input = self.determine_lstm_input(chunk_size)
-        self.lstm = nn.LSTM(input_size=self.lstm_input, hidden_size=10, num_layers=1, dropout=0.4, batch_first=True, bidirectional=True)
-        self.lstm_dropout = nn.Dropout(0.4)
-        
+        self.lstm = nn.LSTM(input_size=self.lstm_input, hidden_size=10, num_layers=2, dropout=0.4, batch_first=True, bidirectional=True)
+        #self.lstm_dropout = nn.Dropout(0.4)
+      
         # Output layer with Softmax activation
         self.output_layer = nn.Sequential(
             #nn.Linear(20,4)
@@ -106,7 +106,7 @@ class HybridCNNLSTM(nn.Module):
         # LSTM layer
         #print(x.shape)
         x, _ = self.lstm(x)
-        x = self.lstm_dropout(x)
+        #x = self.lstm_dropout(x)
         #print(x.shape)
         
         # Output layer
